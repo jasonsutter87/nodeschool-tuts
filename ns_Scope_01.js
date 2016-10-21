9580a51450e76e7b7e9cf5e052457961a6ae4929
Jason Sutter <jasonsutter@Rocko.wavecable.com>
2016-10-21 13:55:20 -0700

    Scope and initial commit

---
+++ ns_Scope_01.js
@@ -0,0 +1,20 @@
+// # Your Mission
+
+// In an empty file, create a function foo which contains one variable lexically
+// scoped named bar.
+
+// Once complete, execute scope-chains-closures verify <your-file.js> to verify your
+// solution.
+
+// ## Notes
+
+//   * [1]: There are also 4 other scopes in the language: Global, `with`, `catch`,
+//   *      and `eval`. These tend not to be used much, so we will ignore them.
+//   * 
+//   * 
+//   * [2]: This workshop will concentrate only on Lexical Scoping.
+
+
+function foo(){
+	var bar
+}
